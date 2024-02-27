db00615557b8ec411e72a1393fc85dd0628105e3
ParkDongJun <yuki@ubisam.com>
2024-02-27 09:08:44 +0900

    ref code 추가

--- tutorial1/src/App.js
+++ tutorial1/src/App.js
@@ -41,12 +41,47 @@ function Goodinput() {
   );
 }
 
+function CountDefEx() {
+
+  const [stateCount, setStateCount] = useState(0);
+  let count = 0
+  const refCount = useRef(0);
+  // 변수처럼 값을 저정할 필요가 있는 변수 단. Ui 보일 필요가 없는애들
+  return (
+    <div>
+      <div>state: {stateCount} </div>
+      <div>변수 : {count}</div>
+      {<div> ref:{refCount.current}</div>}
+
+      <button onClick={() => setStateCount(prev=>prev+1)}>
+        state up
+      </button>
+      <button onClick={() => {
+          count++;
+          console.log("변수", count);
+        }} >
+        변수 up
+      </button>
+      <button onClick={()=>{
+        refCount.current++;
+        console.log("ref",refCount);
+      }}
+      >
+        ref up
+      </button>
+    </div>
+  )
+}
+// useRef 값을 계속 유지
+// useState의 장점과 변수의 장점을 합쳐두게 useRef
 function App() {
   return (
     <div className="App">
       <Badinput/>
       <hr></hr>
       <Goodinput/>
+      <hr></hr>
+      <CountDefEx/>
     </div>
   );
 }
