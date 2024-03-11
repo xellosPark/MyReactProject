07e78c134bfdb1143459f604221d7f7b60d9a36d
yuki <yukihiro@ubisam.com>
2024-03-11 23:20:22 +0900

    flex 작성중

---
+++ flex-box-design/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="flex-item item-1">1</div>
+        <div className="flex-item item-1">1</div>
+
+        <div className="flex-item item-2">2</div>
+
+        <div className="flex-item item-3">3</div>
+
+        <div className="flex-item item-1">1</div>
+        <div className="flex-item item-1">1</div>
+
+        <div className="flex-item item-2">2</div>
+
+        <div className="flex-item item-3">3</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
