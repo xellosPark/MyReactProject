bf4e4b1d8ea19f1d822fe5da522c406f0eee171a
yuki <yukihiro@ubisam.com>
2024-03-11 22:25:13 +0900

    grid 완료

---
+++ grid-design/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import './Grid.css'
+
+function App() {
+  return (
+    <>
+    <div className='container'>
+      
+      <div className='grid-group grid-item-1'>(1-1)</div>
+      <div className='grid-group grid-item-1'>(1-2)</div>
+      <div className='grid-group grid-item-1'>(1-1)</div>
+      <div className='grid-group grid-item-1'>(1-2)</div>
+
+      <div className='grid-group grid-item-2'>(2)</div>
+      <div className='grid-group grid-item-2'>(2)</div>
+
+      <div className='grid-group grid-item-3'>(3)</div> 
+      <div className='grid-group grid-item-3'>(3)</div> 
+    </div> 
+    </>
+  );
+}
+
+export default App;
