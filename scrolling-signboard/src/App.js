ffc100ac37cb624a65aa9c2f73975974b8f32d3b
yuki <yukihiro@ubisam.com>
2024-05-20 22:14:37 +0900

    최종완료

--- scrolling-signboard/src/App.js
+++ scrolling-signboard/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import ScrollText from './ScrollText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 const App = () => {
+  const bellRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const bellElement = bellRef.current;
+    let isPink = true;
+
+    const toggleColor = () => {
+      if (isPink) {
+        bellElement.style.color = 'yellow';
+      } else {
+        bellElement.style.color = 'pink';
+      }
+      isPink = !isPink;
+    };
+
+    const intervalId = setInterval(toggleColor, 500);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
       <div className="column bell-column">
-        <FontAwesomeIcon icon={faBell} size="2x" className="toggle" />
+        <FontAwesomeIcon icon={faBell} size="2x" ref={bellRef} />
       </div>
       <div className="column text-column">
         <p>1열 문자 고정</p>
